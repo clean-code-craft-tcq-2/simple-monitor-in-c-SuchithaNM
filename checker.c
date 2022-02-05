f4c511e28a4bf72da63ed83503d743f86698fdb1
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-02-05 22:41:47 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -9,10 +9,17 @@ int batteryIsOk(float soc , float totalChargeInput, float temp , float chargeRat
 	float chargerate = fpInttBatteryGetChargeRate(chargeRate);
 	return (stateOfCharge && temperature && chargerate);
 }
+int batteryIsNotOk(float soc , float totalChargeInput, float temp , float chargeRate, float (*fpInttBatteryGetSoc)(float,float), float (*fpInttBatteryGetTemp)(float), float (*fpInttBatteryGetChargeRate)(float))
+{	
+	float stateOfCharge = fpInttBatteryGetSoc(soc,totalChargeInput);
+	float temperature = fpInttBatteryGetTemp(temp);
+	float chargerate = fpInttBatteryGetChargeRate(chargeRate);
+	return (stateOfCharge || temperature || chargerate);
+}
 void testBatteryStatus_env()
 {
 	assert(batteryIsOk(32,8,40,0.7,&InttBatteryGetSoc, &InttBatteryGetTemp, &InttBatteryGetChargeRate));
-       // assert(!batteryIsOk(88,22,50,0.9,&InttBatteryGetSoc, &InttBatteryGetTemp, &InttBatteryGetChargeRate)); 
+        assert(!batteryIsOk(88,22,50,0.9,&InttBatteryGetSoc, &InttBatteryGetTemp, &InttBatteryGetChargeRate)); 
 }
 
 int main()
