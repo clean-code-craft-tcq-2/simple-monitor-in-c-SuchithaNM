192ca658759b8bb3c340ececa25dcced8f115bf1
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-02-05 22:36:54 +0530

    Update Bms.h

--- Bms.h
+++ Bms.h
@@ -13,5 +13,6 @@ extern float InttBatteryGetSoc(float totalChargeInput, float soc);
 extern float InttBatteryGetTemp(float temp);
 extern float InttBatteryGetChargeRate(float chargeRate);
 extern int batteryIsOk(float soc , float totalChargeInput, float temp , float chargeRate, float (*fpInttBatteryGetSoc)(float,float), float (*fpInttBatteryGetTemp)(float), float (*fpInttBatteryGetChargeRate)(float));
+extern void testBatteryStatus_env(void);
 
 #endif
