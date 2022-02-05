c0442227b78130304fe1661eac1a898aa185a6b4
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-02-05 20:07:19 +0530

    Update test_BmsStatus.h

--- test_BmsStatus.h
+++ test_BmsStatus.h
@@ -1,8 +1,8 @@
 #ifndef TEST_BMSSTATUS_H
 #define TEST_BMSSTATUS_H
 
-extern void testBatteryStatusOk(float soc ,float temp , float chargeRate);
-extern void testBatteryStatusNotOk(float soc ,float temp , float chargeRate);
-extern void testBatteryStatus_env(void);
+extern float testBatteryStatusOk(float soc ,float temp , float chargeRate);
+extern float testBatteryStatusNotOk(float soc ,float temp , float chargeRate);
+extern float testBatteryStatus_env(void);
 
 #endif
