e688bc2a66a31e5eb3d8f2017855853e8d4151e9
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-02-05 20:17:41 +0530

    Delete test_BmsStatus.h

--- test_BmsStatus.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_BMSSTATUS_H
-#define TEST_BMSSTATUS_H
-
-extern float testBatteryStatusOk(float soc ,float temp , float chargeRate);
-extern float testBatteryStatusNotOk(float soc ,float temp , float chargeRate);
-extern float testBatteryStatus_env(void);
-
-#endif
