f1e7f62ad102c95b51d925fc3f3b94a39762f388
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-02-06 18:50:32 +0530

    Create test_BmsStatus.c

---
+++ test_BmsStatus.c
@@ -0,0 +1,17 @@
+#include "test_BmsStaus.h"
+
+
+int test_batteryIsOk(float expectedSoc , float expectedTotalChargeInput, float expectedTemp , float expectedChargeRate)
+{	
+	batteryIsOk(expectedSoc,expectedTotalChargeInput,expectedTemp,expectedChargeRate,&InttBatteryGetSoc, &InttBatteryGetTemp, &InttBatteryGetChargeRate);
+}
+int test_batteryIsNotOk(float expectedSoc , float expectedTotalChargeInput, float expectedTemp , float expectedChargeRate)
+{	
+	batteryIsNotOk(expectedSoc,expectedTotalChargeInput,expectedTemp,expectedChargeRate,&InttBatteryGetSoc, &InttBatteryGetTemp, &InttBatteryGetChargeRate);
+}
+
+void testBatteryStatus_env()
+{
+	assert(test_batteryIsOk(32,8,40,0.7));
+  assert(test_batteryIsNotOk(88,22,50,0.9)); 
+}
