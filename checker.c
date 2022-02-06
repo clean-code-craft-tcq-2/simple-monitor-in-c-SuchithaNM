6b567467e7e1d342e7b2196c4f947651a9045e28
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-02-06 18:52:25 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -2,20 +2,6 @@
 #include <assert.h>
 #include "Bms.h"
 
-int batteryIsOk(float soc , float totalChargeInput, float temp , float chargeRate, float (*fpInttBatteryGetSoc)(float,float), float (*fpInttBatteryGetTemp)(float), float (*fpInttBatteryGetChargeRate)(float))
-{	
-	float stateOfCharge = fpInttBatteryGetSoc(soc,totalChargeInput);
-	float temperature = fpInttBatteryGetTemp(temp);
-	float chargerate = fpInttBatteryGetChargeRate(chargeRate);
-	return (stateOfCharge && temperature && chargerate);
-}
-int batteryIsNotOk(float soc , float totalChargeInput, float temp , float chargeRate, float (*fpInttBatteryGetSoc)(float,float), float (*fpInttBatteryGetTemp)(float), float (*fpInttBatteryGetChargeRate)(float))
-{	
-	float stateOfCharge = fpInttBatteryGetSoc(soc,totalChargeInput);
-	float temperature = fpInttBatteryGetTemp(temp);
-	float chargerate = fpInttBatteryGetChargeRate(chargeRate);
-	return (stateOfCharge || temperature || chargerate);
-}
 
 int main()
 {
