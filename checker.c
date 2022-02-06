707ed6df04f5e8a25998d3bedb25bbfacf10c4fc
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-02-06 18:11:25 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -4,23 +4,17 @@
 
 int batteryIsOk(float soc , float totalChargeInput, float temp , float chargeRate, float (*fpInttBatteryGetSoc)(float,float), float (*fpInttBatteryGetTemp)(float), float (*fpInttBatteryGetChargeRate)(float))
 {	
-	/*float stateOfCharge = fpInttBatteryGetSoc(soc,totalChargeInput);
+	float stateOfCharge = fpInttBatteryGetSoc(soc,totalChargeInput);
 	float temperature = fpInttBatteryGetTemp(temp);
-	float chargerate = fpInttBatteryGetChargeRate(chargeRate);*/
-	if(((fpInttBatteryGetSoc(soc,totalChargeInput)) == 1) && ((fpInttBatteryGetTemp(temp)) == 1) && ((fpInttBatteryGetChargeRate(chargeRate)) == 1))
-	{
-		return 1;
-	}
+	float chargerate = fpInttBatteryGetChargeRate(chargeRate);
+	return (stateOfCharge && temperature && chargerate);
 }
 int batteryIsNotOk(float soc , float totalChargeInput, float temp , float chargeRate, float (*fpInttBatteryGetSoc)(float,float), float (*fpInttBatteryGetTemp)(float), float (*fpInttBatteryGetChargeRate)(float))
 {	
-	/*float stateOfCharge = fpInttBatteryGetSoc(soc,totalChargeInput);
+	float stateOfCharge = fpInttBatteryGetSoc(soc,totalChargeInput);
 	float temperature = fpInttBatteryGetTemp(temp);
-	float chargerate = fpInttBatteryGetChargeRate(chargeRate);*/
-	if(((fpInttBatteryGetSoc(soc,totalChargeInput)) == 0) || ((fpInttBatteryGetTemp(temp)) == 0) || ((fpInttBatteryGetChargeRate(chargeRate)) == 0))
-	{
-		return 0;
-	}
+	float chargerate = fpInttBatteryGetChargeRate(chargeRate);
+	return (stateOfCharge || temperature || chargerate);
 }
 void testBatteryStatus_env()
 {
