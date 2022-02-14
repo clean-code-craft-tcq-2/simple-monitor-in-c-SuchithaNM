651bae27bd019b176c321d10d8e57efa864f79b2
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-02-14 10:13:17 +0530

    Update test_BmsStaus.h

--- test_BmsStaus.h
+++ test_BmsStaus.h
@@ -4,8 +4,8 @@
 #include <assert.h>
 #include "Bms.h"
 
-extern int test_batteryIsOk(float expectedSoc , float expectedTotalChargeInput, float expectedTemp , float expectedChargeRate);
-extern int test_batteryIsNotOk(float expectedSoc , float expectedTotalChargeInput, float expectedTemp , float expectedChargeRate);
+extern int test_batteryIsOk( float expectedTotalChargeInput, float expectedTemp , float expectedChargeRate);
+extern int test_batteryIsNotOk( float expectedTotalChargeInput, float expectedTemp , float expectedChargeRate);
 extern void testBatteryStatus_env(void);
 
 
